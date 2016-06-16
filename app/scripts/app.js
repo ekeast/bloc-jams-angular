fae237fc4a5b5b8a36281bef0d3260fdbee0273b
Ted Keast <ekeast328@gmail.com>
2016-06-16 12:36:21 -0400

    Checkpoint 3 Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJams', []);
+(function() {
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    function config($stateProvider, $locationProvider) {
+    }
+    
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            templateUrl: '/templates/landing.html'
+    })
+    .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+    });
+    
+    angular 
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
