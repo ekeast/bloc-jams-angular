1ddb57350370abdf566f2593cc374f4e3f17ecd0
Ted Keast <ekeast328@gmail.com>
2016-06-16 13:41:52 -0400

    Assignment 3 Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,11 +11,15 @@
         .state('landing', {
             url: '/',
             templateUrl: '/templates/landing.html'
-    })
-    .state('album', {
-        url: '/album',
-        templateUrl: '/templates/album.html'
-    });
+        })
+        .state('album', {
+            url: '/album',
+            templateUrl: '/templates/album.html'
+        })
+        .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/album.html'
+        });
     
     angular 
         .module('blocJams', ['ui.router'])
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,27 @@
-angular.module('blocJams', []);
+(function() {
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    function config($stateProvider, $locationProvider) {
+    }
+    
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            templateUrl: '/templates/landing.html'
+        })
+        .state('album', {
+            url: '/album',
+            templateUrl: '/templates/album.html'
+        })
+        .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/album.html'
+        });
+    
+    angular 
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
